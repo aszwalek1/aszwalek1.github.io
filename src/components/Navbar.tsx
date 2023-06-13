99d837ab2f812d841fee68ae5fba2d5a9f349d0b
Alicja Szwalek <aszwalek1@sheffield.ac.uk>
2023-06-13 13:58:40 +0100

    added social icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import {useRef} from "react";
-import {FaBars, FaTimes} from "react-icons/fa";
+import {FaBars, FaTimes, FaLinkedin, FaGithub} from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
+
 
 const Navbar = () => {
     const navRef = useRef<HTMLInputElement>(null);
@@ -13,22 +15,41 @@ const Navbar = () => {
         navRef.current?.classList.remove("responsive-nav");
     }
 
+    const {pathname} = useLocation();
+
+
     return (
         <>  
             <nav ref={navRef}>
                 <h3>Alicja Szwalek</h3>
                 <ul >
-                    <li><Link to="/alicja_Szwalek" className="active" onClick={hideNavbar}>Projects</Link></li>
-                    <li><Link to="/alicja_Szwalek/about" onClick={hideNavbar}>About me</Link></li>
-                    <li><Link to="/alicja_Szwalek/cv" onClick={hideNavbar}>CV</Link></li>
+                    <li><Link className={(pathname === '/alicja_Szwalek') ? 'active' : ''}  to="/alicja_Szwalek" onClick={hideNavbar}>Projects</Link></li>
+                    <li><Link className={(pathname === '/alicja_Szwalek/about') ? 'active' : ''} to="/alicja_Szwalek/about" onClick={hideNavbar}>About me</Link></li>
+                    <li><Link className={(pathname === '/alicja_Szwalek/cv') ? 'active' : ''} to="/alicja_Szwalek/cv" onClick={hideNavbar}>CV</Link></li>
                 </ul>
                 <button className="nav-btn nav-close-btn" onClick={hideNavbar}>
-                    <FaTimes/>
+                    <FaTimes />
                 </button>
+                <div className="social-icons">
+                    <a href="https://linkedin.com/in/alicjaszwalek/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="linkedin-icon">
+                        <FaLinkedin />
+                    </a>
+                    <a href="https://github.com/aszwalek1"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="github-icon">
+                        <FaGithub />
+                    </a>
+                </div>
+                
             </nav>
             <button className="nav-btn" onClick={showNavbar}>
                 <FaBars/>
             </button>
+            
         </>
     );
 };
