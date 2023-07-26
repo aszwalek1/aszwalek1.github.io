af7628ad8d92715f3661e927f572e838b408d0f5
Alicja Szwalek <aszwalek1@sheffield.ac.uk>
2023-07-26 19:16:47 +0100

    the about text

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,18 +10,27 @@ const About = () => {
                 <div className="language-4"></div>
             </div>
             <div className="color-dots">
-                <div className="color-dot-1"></div>
+                <div className="dot color-dot-1"></div>
                 <p className="top-language">Java</p>
-                <div className="color-dot-2"></div>
-                <p className="top-language right-language-dot">JavaScript & TypeScript</p>
+                <div className="dot color-dot-2 js"></div>
+                <p className="top-language right-language-dot ">JS/TS/React</p>
             </div>
             <div className="color-dots">
-                <div className="color-dot-3"></div>
+                <div className="dot color-dot-3"></div>
                 <p>Python</p>
-                <div className="color-dot-4"></div>
+                <div className="dot color-dot-4"></div>
                 <p className="right-language-dot">Ruby</p>
             </div>
-            
+            <br></br>
+            <p className="about-me">My name is Alicja Szwalek and I am a third-year Computer Science student at The University of Sheffield. 
+                My primary interests have been 
+                Software Design and Development, Text Processing and Bio-inspired Computing.
+                <br></br>
+                In my free time I learn game development to prepare for my next GameJam and volunteer at Siemens' Digital Innovation Zone to assist other students and small business owners in learning digital skills such as CAD Software (Solid Edge),
+                 3D Scanning and Modelling, and IoT Software (MindSphere)
+                 <br></br>
+                 I am currently searching for a software engineering graduate job
+            </p>
 
         </div>
 
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -4,10 +4,12 @@ const Projects = () => {
     return (
         <div className="page-container" id='slide'>
             <h4 className="page-title">Featured Projects</h4>
+
             <div className="project" >
                 <p className="project-title">Software Hut</p>
-                <p className="work-type">Front-end</p>
-                <p>Software engineering group assignment 2023</p>
+                <p className="work-type">Full-stack</p>
+                <p>Worked in a team of 6 to develop a Ruby on Rails application for <br></br>
+                    the Department of Electronic and Electrical Engineering</p>
                 <p className="language">Ruby on Rails&ensp;</p>
                 <p className="language">JavaScript&ensp;</p>
                 <p className="language">HAML&ensp;</p>
@@ -21,8 +23,18 @@ const Projects = () => {
             <div className="project">
                 <p className="project-title">Java Application</p>
                 <p>Front-end</p>
-                <p>Java group assignment 2023</p>
+                <p>Led the front-end team for a university project that involved <br></br> developing a Java application for a fictitious company</p>
                 <p className="language">Java</p>
+                <div>
+                    <Picture/>
+                </div>
+            </div>
+
+            <div className="project">
+                <p className="project-title">Particle Swarm Optimisation</p>
+                <p>Dissertation project 2023-2024</p>
+                <p>Currently developing a particle swarm optimisation as a teaching tool</p>
+                <p className="language">Python</p>
                 <div>
                     <Picture/>
                 </div>
@@ -39,7 +51,7 @@ const Projects = () => {
                 <Picture/>
                
             </div>
-
+            <p className='visit-github'>Visit my Github to see the code</p>
         </div>
 
     );
