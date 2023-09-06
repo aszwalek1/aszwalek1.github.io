a23872f87af7da0afcf2984a53069130b2c8130e
Alicja Szwalek <aszwalek1@sheffield.ac.uk>
2023-09-06 21:11:20 +0100

    added a project, changed descriptions

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -6,23 +6,50 @@ const Projects = () => {
         <div className="page-container" id='slide'>
             <div className='columns-container'>
                 <div className='projects-left-column'>
+                    <div>
+                        <SoftwareHut_Image/>
+                    </div>
+                    <div className='project-picture-2'>
+                        <SystemsDesign_Image/>
+                    </div>
+                    <div className='project-picture-3'>
+                        <SystemsDesign_Image/>
+                    </div>
+                    
+                </div>
+                <div className='projects-right-column'>
                     <h4 className="page-title">Featured Projects</h4>
                     
                     <div className="project" >
                         <p className="project-title">Software Hut</p>
-                        <p>Worked in a team of 6 to develop a Ruby on Rails application for <br></br>
-                            the Department of Electronic and Electrical Engineering</p>
-                        <p>Used User Stories and Kanban to manage the development process</p>
+                        <p>Led the front-end team and assisted team members in learning the required skills</p>
+                        <p>Managed diverse stages of software development lifecycle: requirement construction through customer communication, agile software development, documentation, and testing. </p>
+                        <p>Developed a web application utilising Ruby on Rails, HTML5, CSS, PostgreSQL, and JavaScript. Ensured quality through unit tests and BDD testing. </p>
                         <p className="language">Ruby on Rails&ensp;</p>
                         <p className="language">JavaScript&ensp;</p>
                         <p className="language">HAML&ensp;</p>
-                        <p className="language">CSS</p>  
+                        <p className="language">CSS&ensp;</p>  
+                        <p className="language">PosgreSQL</p>  
+                    </div>
+                    <div className="project">
+                        <p className="project-title">Task Management Application</p>
+                        <p>Created a Java Spring Boot application with React frontend and MongoDB</p>
+                        <p>Designed and implemented key features including CRUD operations, filtering, password hashing, dashboard, and user registration.</p>
+                        <p>Rigorously tested the application using both unit and integration tests (JUnit and Spring's testing framework).</p>
+                        <p className="language">Java&ensp;</p>
+                        <p className="language">React&ensp;</p>
+                        <p className="language">MongoDB&ensp;</p>
+                        <p className="language">HTML&ensp;</p>
+                        <p className="language">CSS</p>
                     </div>
 
                     <div className="project">
-                        <p className="project-title">Java Application</p>
-                        <p>Developed a Java application for a fictitious company</p>
-                        <p className="language">Java</p>
+                        <p className="project-title">Bike Selling Company Application</p>
+                        <p>Cooperated in a team to develop a Java and MySQL application for a fictitious company</p>
+                        <p>Enhanced UML class diagram and state machine skills</p>
+                        <p>Employed Kanban and pair programming methodologies to drive project development</p>
+                        <p className="language">Java&ensp;</p>
+                        <p className="language">MySQL</p>
                     </div>
                     
                     <div className="project">
@@ -36,18 +63,7 @@ const Projects = () => {
 
                     <p className='visit-github'>Visit my Github to see the code</p>
                 </div>
-                <div className='projects-right-column'>
-                    <div>
-                        <SoftwareHut_Image/>
-                    </div>
-                    <div>
-                        <SystemsDesign_Image/>
-                    </div>
-                    <div>
-                        <SystemsDesign_Image/>
-                    </div>
-                    
-                </div>
+                
             </div>
             
 
